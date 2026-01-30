77447730ede59b371af06c61924c5fa21bb84c4e
thiago-dev <proterledesma@gmail.com>
2026-01-30 17:48:37 -0300

    incremental merge terminado

--- etl/sql/merge_dim_player.sql
+++ etl/sql/merge_dim_player.sql
@@ -1,24 +1,43 @@
 MERGE INTO dim_player d
 USING (
-    SELECT DISTINCT
+    SELECT
         player_id,
-        first_name AS player_name,
-        age
+        player_name,
+        first_name,
+        last_name,
+        age,
+        nationality,
+        snapshot_date
     FROM staging.stg_players_incremental_clean
 ) s
 ON d.player_id = s.player_id
 
-WHEN NOT MATCHED THEN
-INSERT (
+WHEN MATCHED
+ AND s.snapshot_date > d.snapshot_date
+THEN UPDATE SET
+    player_name   = s.player_name,
+    firstname     = s.first_name,
+    lastname      = s.last_name,
+    age           = s.age,
+    nationality   = s.nationality,   -- ✅ ACÁ
+    snapshot_date = s.snapshot_date
+
+WHEN NOT MATCHED
+THEN INSERT (
     player_id,
     player_name,
-    age
+    firstname,
+    lastname,
+    age,
+    nationality,      -- ✅ ACÁ
+    snapshot_date
 )
 VALUES (
     s.player_id,
     s.player_name,
-    s.age
+    s.first_name,
+    s.last_name,
+    s.age,
+    s.nationality,
+    s.snapshot_date
 );
-
-
-
--- etl/sql/stg_players_incremental_clean.sql
+++ etl/sql/stg_players_incremental_clean.sql
@@ -1,13 +1,14 @@
-CREATE OR REPLACE TABLE staging.stg_players_incremental_clean AS
-SELECT
-    p.id              AS player_id,
-    p.name            AS player_name,
-    p.age             AS age,
-    p.nationality     AS nationality,
-    p.height          AS height,
-    p.weight          AS weight,
-    r.snapshot_date
-FROM staging.stg_players_incremental r,
-     UNNEST(r.response) AS t(player),
-     UNNEST(t.player) AS p;
+CREATE OR REPLACE TABLE stg_players_incremental_clean AS
+SELECT DISTINCT
+    r.unnest.player.id          AS player_id,
+    r.unnest.player.name        AS player_name,
+    r.unnest.player.firstname   AS first_name,
+    r.unnest.player.lastname    AS last_name,
+    r.unnest.player.age         AS age,
+    r.unnest.player.nationality AS nationality,
+    s.snapshot_date
+FROM stg_players_incremental s
+CROSS JOIN UNNEST(s.response) AS r;
+
+
 
